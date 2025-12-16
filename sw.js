59debfc1e4b905799dd473f788976af8b8450d22
guttoand-svg <gutto.and@gmail.com>
2025-12-16 12:03:46 -0400

    sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "serra-alegre-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "https://fzserraalegre.blogspot.com/"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
